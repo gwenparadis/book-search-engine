ae9e37dbf3048cdb2a31957bbe92dc6deeac7e81
gwenparadis <gwendelynparadis2@gmail.com>
2023-03-12 21:41:00 -0400

    typeDefs rough draft

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,23 +5,33 @@ const typeDefs = gql`
     _id: _id
     username: String
     email: String
-    password: String
-    savedBooks: In
+    bookCount: Int
+    savedBooks: [Book]
   }
 
   type Book {
-    _id: id
-    authors: String
-    description: String
     bookId: String
+    authors: [String]
+    description: String
+    title: String
     image: String
     link: String
-    title: String
+  }
+
+  type Auth {
+    token:
+    user: User
   }
 
   type Query {
-    users: [User]
-    books: [Book]
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+    removeBook(bookId: String!): User
   }
 `;
 
