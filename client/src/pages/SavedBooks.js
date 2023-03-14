fda81df177ffd789ee2fd130673c1052cfab1aae
gwenparadis <gwendelynparadis2@gmail.com>
2023-03-14 13:35:59 -0400

    get me query front end

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
 import { useQuery, useMutation } from "@apollo/client";
-import { QUERY_ME } from "../utils/queries";
+import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
 import { removeBookId } from "../utils/localStorage";
 
 import Auth from "../utils/auth";
 
 const SavedBooks = () => {
-  const { loading, data } = useQuery(QUERY_ME);
+  const { loading, data } = useQuery(GET_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
@@ -32,8 +32,8 @@ const SavedBooks = () => {
       }
 
       removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.error(error);
     }
   };
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,10 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-  query me {
+  QueryMe {
     me {
       _id
       username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
     }
   }
 `;
