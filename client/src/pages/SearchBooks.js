a92df57c7267c44359857ff44c4f940e51dd879c
gwenparadis <gwendelynparadis2@gmail.com>
2023-03-15 16:42:58 -0400

    auth typo

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,7 +5,7 @@ import Auth from "../utils/auth";
 import { saveBook, searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
-const SearchBooks = () => {
+function SearchBooks() {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -151,6 +151,6 @@ const SearchBooks = () => {
       </Container>
     </>
   );
-};
+}
 
 export default SearchBooks;
